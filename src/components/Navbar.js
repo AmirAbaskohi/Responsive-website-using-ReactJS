009cad93b667bf12db91072ab8953693abf894d3
AmirHosseinMissMe <amirhossein2324236@gmail.com>
2020-09-06 21:01:21 +0430

    closeMobileMenu functionaility to the buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -32,7 +32,7 @@ function Navbar() {
         <IconContext.Provider value={{color: '#fff' }}>
             <div className="navbar">
                 <div className="navbar-container container">
-                    <Link to="/" className="navbar-logo">
+                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
                         <MdFingerprint className="navbar-icon" />
                         LAVISH
                     </Link>
@@ -42,27 +42,27 @@ function Navbar() {
                 </div>
                 <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                     <li className="nav-item">
-                        <Link to="/" className='nav-links'>
+                        <Link to="/" className='nav-links' onClick={closeMobileMenu}>
                             Home
                         </Link>
                     </li>
                     <li className="nav-item">
-                        <Link to="/services" className='nav-links'>
+                        <Link to="/services" className='nav-links' onClick={closeMobileMenu}>
                             Services
                         </Link>
                     </li>
                     <li className="nav-item">
-                        <Link to="/products" className='nav-links'>
+                        <Link to="/products" className='nav-links' onClick={closeMobileMenu}>
                             Products
                         </Link>
                     </li>
                     <li className="nav-btn">
                         {button ? (
-                            <Link to="/sign-up" className="btn-link">
+                            <Link to="/sign-up" className="btn-link" >
                                 <Button buttonStyle="btn--outline">SIGN UP</Button>
                             </Link>
                         ):(
-                            <Link to="/sign-up" className="btn-link">
+                            <Link to="/sign-up" className="btn-link" onClick={closeMobileMenu}>
                                 <Button buttonStyle="btn--outline"
                                         buttonSize="btn--mobile"
                                 >SIGN UP</Button>
