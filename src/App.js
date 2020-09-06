5684a5e52e0b24c1f87fec5a2c34582c2cc188f1
AmirHosseinMissMe <amirhossein2324236@gmail.com>
2020-09-06 18:26:23 +0430

    Navbar and button components and some styles for Buttton added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Helllllllo</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" />
+      </Switch>
+    </Router>
   );
 }
 
