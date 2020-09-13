8c37d23ce559651c80b27f51cde6dc5c74e51738
AmirHosseinMissMe <amirhossein2324236@gmail.com>
2020-09-13 14:25:06 +0430

    Hero section added

---
+++ src/components/pages/HomePage/Data.js
@@ -0,0 +1,12 @@
+export const homeObjOne = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Exclusive Access',
+    headline: 'Unlimited Transactions with zero fees',
+    description: 'Get access to out exclusive diamond card that allows you to send unlimited transactioons without getting charged any fees',
+    buttonLabel: 'Get Started',
+    img: 'images/svg-1.svg',
+    alt: 'Credit Card',
+    imgStart: ''
+};
